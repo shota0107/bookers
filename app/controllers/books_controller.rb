bc1eab6053e60ed1662365527f8382d412386b37
EC2 Default User <ec2-user@ip-172-31-20-229.ec2.internal>
2022-06-01 07:17:00 +0000

    0601

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,16 +7,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "successfully"
-    if@book.save
-    redirect_to book_path(book.id)
-    else
-    render :new
-    end 
-  end
+  # def create
+  #   book = Book.new(book_params)
+  #   book.save
+  #   flash[:notice] = "successfully"
+  #   redirect_to book_path(book.id)
+  # end
 
   def show
     @book = Book.find(params[:id])
@@ -26,16 +22,40 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  # def update
+  #   book = Book.find(params[:id])
+  #   book.update(book_params)
+  #   flash[:notice] = "successfully"
+  #   redirect_to book_path(book.id)
+  # end
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
     flash[:notice] = "successfully"
-    redirect_to book_path(book.id)
+    else
+    # @book = Book.find(params[:id])
+     render :edit
+    end
+  end
+
+  def create
+   @book = Book.new(book_params)
+   if @book.save
+     flash[:notice] = "successfully"
+     redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render :index
+   end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "successfully"
     redirect_to books_path
   end
 
